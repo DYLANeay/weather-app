65d33e0c9793b9ff208d254e9bec8513f880d2a5
DYLANeay <dylan.eray6@gmail.com>
2025-03-31 17:33:22 +0200

    generate api key and first project form

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+console.log('hi');
+
+//API key connection link : https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=KCTHAJZQZNWSG2EP9Y3X7XBBB
