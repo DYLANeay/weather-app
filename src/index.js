a795198f1840ad22e011c711d3474785820ea721
DYLANeay <dylan.eray6@gmail.com>
2025-04-01 11:34:24 +0200

    solve bug about the changing unit btn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './style.css';
-import { handleFormSubmit, hideLoading } from './ui.js';
+import { changeUnit, handleFormSubmit, hideLoading } from './ui.js';
 import { fetchWeatherData } from './api.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   // Get DOM elements
   const weatherForm = document.getElementById('weather-form');
   const locationInput = document.getElementById('location-input');
+  const celsiusBtn = document.getElementById('celsius-btn');
+  const fahrenheitBtn = document.getElementById('fahrenheit-btn');
 
   // API key - replace with your Visual Crossing API key
   const API_KEY = 'KCTHAJZQZNWSG2EP9Y3X7XBBB';
@@ -26,7 +28,27 @@ document.addEventListener('DOMContentLoaded', function () {
       hideLoading(); //hide Loading once it fetched !
     } catch (error) {
       console.error('Error fetching weather data:', error);
-      showError('Failed to fetch weather data. Please try again.');
+      // showError('Failed to fetch weather data. Please try again.');
+    }
+  });
+
+  // Handle change unit btn
+
+  celsiusBtn.addEventListener('click', () => {
+    currentUnit = 'celsius';
+    celsiusBtn.classList.add('active');
+    fahrenheitBtn.classList.remove('active');
+    if (weatherData) {
+      changeUnit(currentUnit, weatherData);
+    }
+  });
+
+  fahrenheitBtn.addEventListener('click', () => {
+    currentUnit = 'fahrenheit';
+    fahrenheitBtn.classList.add('active');
+    celsiusBtn.classList.remove('active');
+    if (weatherData) {
+      changeUnit(currentUnit, weatherData);
     }
   });
 });
